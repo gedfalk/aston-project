5019c5e0641634f3e9e4cbf685b33649bf340c6e
gedfalk <blite.co.stn@gmail.com>
2025-12-21 02:31:14 +0300

    feat: подключен lombok, создан тестовый Entity User

--- src/main/java/dev/gedfalk/astonproject/Main.java
+++ src/main/java/dev/gedfalk/astonproject/Main.java
@@ -1,7 +1,21 @@
 package dev.gedfalk.astonproject;
 
+import dev.gedfalk.astonproject.entity.User;
+
+import java.time.LocalDateTime;
+
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         System.out.println("Hell");
+
+        User user = User.builder()
+                .name("Eugene")
+                .email("bla-bla@bla.com")
+                .age(30)
+                .created_at(LocalDateTime.now())
+                .build();
+
+        System.out.println(user);
+        System.out.println(user.getCreated_at());
     }
 }
---
+++ src/main/java/dev/gedfalk/astonproject/entity/User.java
@@ -0,0 +1,20 @@
+package dev.gedfalk.astonproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDateTime created_at;
+}
