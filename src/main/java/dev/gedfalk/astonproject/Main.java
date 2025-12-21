9c1ed8c7373891c22dee7d888345d6cd8e2bd63c
gedfalk <blite.co.stn@gmail.com>
2025-12-21 03:01:39 +0300

    feat: создан прототип консоли

--- src/main/java/dev/gedfalk/astonproject/Main.java
+++ src/main/java/dev/gedfalk/astonproject/Main.java
@@ -1,21 +1,11 @@
 package dev.gedfalk.astonproject;
 
-import dev.gedfalk.astonproject.entity.User;
-
-import java.time.LocalDateTime;
+import dev.gedfalk.astonproject.console.ConsoleInterface;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hell");
-
-        User user = User.builder()
-                .name("Eugene")
-                .email("bla-bla@bla.com")
-                .age(30)
-                .created_at(LocalDateTime.now())
-                .build();
-
-        System.out.println(user);
-        System.out.println(user.getCreated_at());
+        ConsoleInterface console = new ConsoleInterface();
+        console.run();
     }
 }
---
+++ src/main/java/dev/gedfalk/astonproject/console/ConsoleInterface.java
@@ -0,0 +1,68 @@
+package dev.gedfalk.astonproject.console;
+
+import dev.gedfalk.astonproject.entity.User;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class ConsoleInterface {
+    private final Scanner scanner;
+
+    public ConsoleInterface() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void printMenu() {
+        System.out.println();
+        System.out.println("Выберите из нижеперечисленного (цифрой от 1 до 6):");
+        System.out.println("1. create");
+        System.out.println("2. read");
+        System.out.println("3. update");
+        System.out.println("4. delete");
+        System.out.println("5. Вывести весь список");
+        System.out.println("6. Выход");
+    }
+
+    public void run() {
+        while (true) {
+            printMenu();
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    // TODO: временная заглушка. Удалить!!!
+                    createUser();
+                    break;
+                case "2":
+                    // TODO: readUser
+                    break;
+                case "3":
+                    // TODO: updateUser
+                    break;
+                case "4":
+                    // TODO: deleteUser
+                    break;
+                case "5":
+                    // TODO: вывести всех
+                    break;
+                case "6":
+                    System.out.println("Завершение программы...");
+                    return;
+                default:
+                    System.out.println("Некорректный ввод.\nВведите цифру от 1 до 6");
+            }
+        }
+    }
+
+    // TODO: временная заглушка. Удалить!!!
+    public void createUser() {
+        User user = User.builder()
+                .name("Eugene")
+                .email("bla-bla@bla.com")
+                .age(30)
+                .created_at(LocalDateTime.now())
+                .build();
+
+        System.out.println(user);
+    }
+}
