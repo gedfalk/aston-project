0990e9bd3923739a678a6c3f7baf4b74033357b8
gedfalk <blite.co.stn@gmail.com>
2025-12-29 00:42:21 +0300

    feat: создание service-слоя

---
+++ src/main/java/dev/gedfalk/astonproject/service/UserService.java
@@ -0,0 +1,71 @@
+package dev.gedfalk.astonproject.service;
+
+import dev.gedfalk.astonproject.dao.UserDAO;
+import dev.gedfalk.astonproject.entity.User;
+
+import java.time.LocalDateTime;
+
+public class UserService {
+    private final UserDAO userDao;
+
+    public UserService(UserDAO userDao) {
+        this.userDao = userDao;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        String verifiedName = name.trim();
+        if (verifiedName.length() < 2) {
+            throw new IllegalArgumentException("Имя должно быть больше 2 символов");
+        }
+        if (verifiedName.length() > 50) {
+            throw new IllegalArgumentException("Слииишком длинное имя");
+        }
+    }
+
+    private void validateEmail(String email) {
+        if (email == null || email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Мыло обязательно");
+        }
+        String verifiedEmail = email.trim();
+        if (!verifiedEmail.contains("@") || !verifiedEmail.contains(".")) {
+            throw new IllegalArgumentException("Некорректный формат");
+        }
+        if (verifiedEmail.length() > 50) {
+            throw new IllegalArgumentException("Cлиииишком длинное мыло");
+        }
+    }
+
+    private void validateAge(Integer age) {
+        if (age == null) {
+            throw new IllegalArgumentException("Возраст нужно указать обязательно");
+        }
+        if (age < 0) {
+            throw new IllegalArgumentException("Возраст должен быть больше 0");
+        }
+        if (age > 120) {
+            throw new IllegalArgumentException("Возраст должен быть меньше 120");
+        }
+    }
+
+    public User createUser(String name, String email, Integer age) {
+        validateName(name);
+        validateEmail(email);
+        validateAge(age);
+
+        if (userDao.existByEmail(email.trim().toLowerCase())) {
+            throw new IllegalArgumentException("Пользователь с мылом " + email + " уже есть");
+        }
+
+        User user = User.builder()
+                .name(name.trim())
+                .email(email.trim().toLowerCase())
+                .age(age)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return userDao.save(user);
+    }
+}
