e683ae87bf8b6a49e02f3be7073596dbfb9c7778
gedfalk <blite.co.stn@gmail.com>
2026-01-28 00:35:42 +0300

    docs: задокументирован HealthController для проверки работоспособности swagger

--- src/main/java/dev/gedfalk/astonproject/controller/HealthController.java
+++ src/main/java/dev/gedfalk/astonproject/controller/HealthController.java
@@ -1,12 +1,21 @@
 package dev.gedfalk.astonproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health Check", description = "Проверка базовой работоспособности API")
 public class HealthController {
 
     @GetMapping("/health")
+    @Operation(summary = "Проверка 'здоровья' приложения")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Приложение работает корреткно"
+    )
     public String health() {
         return "Всё чики-пуки!";
     }
