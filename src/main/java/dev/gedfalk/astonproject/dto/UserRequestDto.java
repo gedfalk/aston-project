7f325736a9199b43ecda59cba691015dec93ce6d
gedfalk <blite.co.stn@gmail.com>
2026-01-28 02:09:59 +0300

    docs: задокументированы UserRequestDto и UserResponseDto

--- src/main/java/dev/gedfalk/astonproject/dto/UserRequestDto.java
+++ src/main/java/dev/gedfalk/astonproject/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package dev.gedfalk.astonproject.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,14 +17,21 @@ import lombok.NoArgsConstructor;
 public class UserRequestDto {
 
     @NotBlank(message = "Требуется ввести имя")
+    @Schema(description = "Имя пользователя", example = "Джо Роган")
     private String name;
 
     @NotBlank(message = "Требуется ввести почтовый ящик")
     @Email(message = "Некорректный формат почтового ящика")
+    @Schema(description = "Почтовый ящик пользователя", example = "joerogan@gmail.com")
     private String email;
 
     @NotNull(message = "Требуется ввести возраст")
     @Positive(message = "Возраст должен быть больше нуля")
+    @Schema(
+            description = "Возраст пользователя",
+            example = "66",
+            minimum = "1"
+    )
     private Integer age;
 
 }
--- src/main/java/dev/gedfalk/astonproject/dto/UserResponseDto.java
+++ src/main/java/dev/gedfalk/astonproject/dto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package dev.gedfalk.astonproject.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDto {
+
+    @Schema(description = "Уникальный идентификатор пользователя", example = "666")
     private Integer id;
+
+    @Schema(description = "Имя пользователя", example = "Джо Роган")
     private String name;
+
+    @Schema(description = "Почтовый ящик пользователя", example = "joerogan@gmail.com")
     private String email;
+
+    @Schema(description = "Возраст пользователя", example = "66")
     private Integer age;
+
+    @Schema(description = "Дата и время создания пользователя. Задается автоматически системой")
     private LocalDateTime createdAt;
 }
